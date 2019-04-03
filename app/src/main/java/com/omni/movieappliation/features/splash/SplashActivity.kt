ef9ef4efd0d5af688db6d6a49907bb06e7c9cca0
Omni <engomneya.osman@gmail.com>
2019-04-04 00:38:32 +0200

    adding Splash Activity

---
+++ app/src/main/java/com/omni/movieappliation/features/splash/SplashActivity.kt
@@ -0,0 +1,13 @@
+package com.omni.movieappliation.features.splash
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.omni.movieappliation.R
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
